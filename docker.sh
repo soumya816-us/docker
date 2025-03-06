be1af2be5d479163a6f0663619594eceebf27750
soumyareddy <soumyareddy816@gmail.com>
2025-03-05 22:00:19 -0500

    docker

--- docker.sh
+++ docker.sh
@@ -18,9 +18,9 @@ usermod -aG docker ec2-user
 #kubectl installation
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-sudo mv kubectl /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/kubectl
 
 #eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
-sudo mv /tmp/eksctl /usr/local/bin
+mv /tmp/eksctl /usr/local/bin
